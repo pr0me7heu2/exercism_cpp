ac4e09aaf4f06065a6e8976fa041ee668e5ae581
pr0me7heu2 <brbrasher@gmail.com>
2019-05-11 08:58:17 -0400

    Initial commit

---
+++ hello-world/hello_world.cpp
@@ -0,0 +1,25 @@
+//
+// Created by brbrasher on 5/11/19.
+//
+
+#include <iostream>
+
+void print_hi();
+//prints hello world to std out
+
+int main()
+{
+    print_hi();
+
+    return 0;
+}
+
+void print_hi()
+{
+    using namespace std;
+
+    cout << "Hello, World!" << endl;
+}
+
+
+
